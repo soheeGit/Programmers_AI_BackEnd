0e219d7740e417d30629b20854a502ba2ca2ab94
soheeGit <63wlsthgml@gmail.com>
2025-04-09 12:52:55 +0900

    250409

--- programming/boot-thymeleaf/src/main/java/org/example/bootthymeleaf/controller/MainController.java
+++ programming/boot-thymeleaf/src/main/java/org/example/bootthymeleaf/controller/MainController.java
@@ -1,6 +1,7 @@
 package org.example.bootthymeleaf.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bootthymeleaf.model.dto.WordForm;
 import org.example.bootthymeleaf.model.entity.Word;
 import org.example.bootthymeleaf.model.repository.WordRepository;
 import org.springframework.stereotype.Controller;
@@ -17,16 +18,36 @@ public class MainController {
 
     @GetMapping
     public String index(Model model, @RequestParam(required = false) String message) {
+//        Word word = new Word();
+//        word.setText("고양이");
+//        wordRepository.save(word);
         model.addAttribute("words", wordRepository.findAll());
-        model.addAttribute("message", message);
-        return "index";
+//        model.addAttribute("message", message);
+        // 타임리프에서 이미 폼을 이미 정의된 걸로 쓰려면 Model을 통해서 전달해야합니다
+        model.addAttribute("wordForm", new WordForm()); // 주입함!
+        return "index"; // forward
+    }
+
+    @PostMapping("/word")
+    public String addWord(WordForm wordForm, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message", "끝말잇기 추가");
+        Word word = new Word();
+        word.setText(wordForm.getWord());
+        wordRepository.save(word);
+        return "redirect:/";
     }
 
     @PostMapping("/reset")
     public String resetWords(RedirectAttributes redirectAttributes) {
-        wordRepository.deleteAll();
-        redirectAttributes.addAttribute("message", "전체 삭제되었습니다!");
-        redirectAttributes.addFlashAttribute("message2", "전체 DELETE 되었습니다!");
+        wordRepository.deleteAll(); // 고양이들아 안녕!
+        // Model을 쓸 수 없는 이유 -> forward
+//        redirectAttributes.addAttribute("message", "전체 삭제되었습니다!");
+        // 주소창을 통해서 전달했기에 Parameter로 해서 받아서 Model로 넣어줘야했다면...
+//        redirectAttributes.addFlashAttribute("message2", "전부 DELETE 되었습니다!");
+        redirectAttributes.addFlashAttribute("message", "끝말잇기 기록 리셋");
+        // 바로 model로 넣어준다...
+        // message -> Parameter
+        // 주소 창에 노출이 된다
         return "redirect:/";
     }
 }
